e18d2e5f8f558e62197d1fb070241b126518fe5a
Eduard <knoxwillesp@gmail.com>
2025-01-12 23:53:56 +0300

    Update Module_13_2.py

--- Module_13_2.py
+++ Module_13_2.py
@@ -9,6 +9,7 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 @dp.message_handler(commands=['start'])
 async def start(message):
     print('Привет! Я бот помогающий твоему здоровью')
+
 @dp.message_handler()
 async def all_messages(message):
     print('Введите команду /start, чтобы начать общение')
